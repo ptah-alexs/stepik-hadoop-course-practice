00d840907634f5242af007151e92b96f6556edb7
Aleksandr Opachev <alexs6@mail.ru>
2022-09-10 20:20:18 +0400

    решение для шага 4.2.5

---
+++ 4.2.5-reducer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "fmt"
+    "strings"
+)
+
+func main() {
+    tempKey := ""
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        sVals := strings.Split(scanner.Text(), "\t")
+        if tempKey != sVals[0] {
+            //fmt.Println(sVals[0])
+            tempKey = sVals[0]
+        } else {
+            fmt.Println(sVals[0])
+        }
+    }
+    if scanner.Err() != nil {
+        fmt.Printf("error: %s\n", scanner.Err())
+    }
+}
