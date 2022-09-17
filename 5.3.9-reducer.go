61a9479512714e3280bf6267b14041ba95f2eb82
Aleksandr Opachev <alexs6@mail.ru>
2022-09-17 20:05:25 +0400

    решение шага 5.3.9

---
+++ 5.3.9-reducer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "fmt"
+)
+
+func main() {
+    tempKey := ""
+    dfval := 0.0
+    dvert := ""
+    dparts := ""
+    dvrtx := ""
+    dpr := 0.0
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        fmt.Sscanf(scanner.Text(),"%s\t%f\t{%s}", &dvert, &dfval, &dparts)
+        if tempKey != "" && dvert != tempKey {
+            fmt.Printf("%s\t%.3f\t{%s}\n", tempKey, dpr, dvrtx)
+            dpr = 0.0
+            tempKey = dvert
+            dvrtx = ""
+            dvrtx = dparts[:len(dparts)-1]
+            if dparts[:len(dparts)-1] != "" {
+            } else {
+                dpr += dfval
+            }
+        } else {
+            
+            if dparts[:len(dparts)-1] != "" {
+                dvrtx = dparts[:len(dparts)-1]
+            } else {
+                dpr += dfval
+            }
+            tempKey = dvert
+        }
+    }
+    if scanner.Err() != nil {
+        fmt.Printf("error: %s\n", scanner.Err())
+    }
+    if tempKey != "" {
+        dvrtx = dparts[:len(dparts)-1]
+        fmt.Printf("%s\t%.3f\t{%s}\n", tempKey, dpr, dvrtx)
+    }
+}
